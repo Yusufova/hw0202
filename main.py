0041f09c0b040b859a3fd0c75e3909aa71749fd5
Yusufova <yusufova.odina11@gmail.com>
2024-02-02 21:46:17 +0500

    h|w

---
+++ main.py
@@ -0,0 +1,101 @@
+#2
+# friends = ["Adam", "Sarah", "Malcolm"]
+#
+# i = 0
+# while i < len(friends):
+#     x = friends[i][0]
+#     i += 1
+#     print(x)
+#3
+# lst = ["banana", "apple", "peach", "strawberry"]
+#
+# res = []
+# i = 0
+# while i < len(lst):
+#     x = lst[i]
+#     i += 1
+#     tst = input("enter any fruits: ")
+#     if tst == x:
+#         res.append(1)
+#     else:
+#         res.append(-1)
+# print(res)
+#4
+# l = [3, 2, 5]
+# d = [5, 3, 7, 9, 2]
+#
+# i = 0
+
+# while i < len(l):
+#     x = l[i]
+# while i < len(d):
+#     y = d[i]
+#     if x in d:
+#         print(True)
+#     else:
+#         print(False)
+
+#5
+
+# p = [1, 0, 1, 1, 0]
+#
+# i = 0
+# while i < len(p):
+#     y = p[i]
+#     i += 0
+#     if y == 1:
+#         print(True)
+#         break
+#     elif y == 0:
+#         print(False)
+#
+
+#6
+
+# s = [6, 2, 3, 4, 8, 9]
+# d = s.sort()
+# e = d[::-1]
+# print(e)
+
+# 7
+# names = {
+#     "st1": "Steve",
+#     "st2": "Becky",
+#     "st3": "John",
+# }
+# for value in names.values():
+#     print(sorted(value))
+
+#8
+# setify = [1, 3, 3, 5, 5, 5]
+# print(list(set(setify)))
+
+#1
+# lst1 = [1, 2, 3, 4, 5, 6, 8]
+# lst2 = [1, 2, 3, 4, 5, 6, 8]
+#
+# def simon_says(lst1, lst2):
+# 	return lst1[:-1] == lst2[1:]
+
+# #9
+# i = input("Enter your name:")
+# f = "Hello"
+# print(f"Hello {i}")
+
+#10
+# volume = ([2, 2, 2], [1, 1, 1])
+# sum = 0
+# i = 0
+# while i < len(volume):
+#     x = volume[i]
+#     i += 1
+#     m = 1
+#     while m < len(x):
+#         n = x[m]
+#         m += 1
+#         m = m * n
+#         sum += m
+# print(sum)
+
+
+
